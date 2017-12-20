6f68f5c77ad6951b7955c4bbf42901fb381b97c2
Deetss <dylan817@live.com>
2017-12-20 22:15:22 +0000

    Changes to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,15 @@
 class User < ApplicationRecord
   after_create :send_welcome_email
   
-  has_attached_file :picture, styles: {medium: "300x300>", thumb: "100x100>" }, default_url: "/images/missing.png"
-  validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
-  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable, omniauth_providers: %i[facebook]
          
+    
+  has_attached_file :picture, styles: {medium: "300x300>", thumb: "100x100>" }, default_url: "/images/missing.png"
+  validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
+  
   has_many :relationships
        
   has_many :sent_requests, class_name: "Relationship",
@@ -65,7 +66,7 @@ class User < ApplicationRecord
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
       user.name = auth.info.name   # assuming the user model has a name
-      # user.image = auth.info.image # assuming the user model has an image
+      user.picture = auth.info.image # assuming the user model has an image
       # If you are using confirmable and the provider(s) you use validate emails, 
       # uncomment the line below to skip the confirmation emails.
       # user.skip_confirmation!
