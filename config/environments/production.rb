d8664625f2c560a7e5ecff89ec6a5c176f65e25d
Deetss <dylan817@live.com>
2017-12-20 22:47:33 +0000

    Add region to paperclip config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,11 +74,12 @@ Rails.application.configure do
   config.paperclip_defaults = {
     storage: :s3,
     s3_credentials: {
-      bucket: ENV["AWS_S3_BUCKET"],
-      access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-      secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]
-      }
-    } 
+      bucket: ENV.fetch('AWS_S3_BUCKET'),
+      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+      s3_region: ENV.fetch('AWS_REGION'),
+    }
+  }
   
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
